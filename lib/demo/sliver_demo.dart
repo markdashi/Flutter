43e4fe3366eaeee3ef023bc7cbb19f83720d9574
王俊斐 <wangjf@sengled.com>
2019-01-02 11:19:13 +0800

    Add sliverAppBar

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -7,6 +7,26 @@ class SliverDemo extends StatelessWidget {
       return Scaffold(
         body: CustomScrollView(
           slivers: <Widget>[
+            SliverAppBar(
+              // title: Text('Hello world'),
+              // pinned: true,
+              floating: true,
+              expandedHeight: 178.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(
+                  'Tour Flutter'.toUpperCase(),
+                  style:TextStyle(
+                    fontSize:15.0,
+                    letterSpacing:3.0,
+                    fontWeight: FontWeight.w400
+                  )
+                  ),
+                  background: Image.network(
+                    'https://resources.ninghao.net/images/overkill.png',
+                    fit: BoxFit.cover,
+                    ),
+              ),
+            ),
              SliverSafeArea(
                sliver: SliverPadding(
                padding: EdgeInsets.all(8.0),
@@ -24,17 +44,51 @@ class SliverListDemo extends StatelessWidget {
     Widget build(BuildContext context) {
       return SliverList(
               delegate: SliverChildBuilderDelegate((BuildContext context,int index){
-                return Material(
-                    borderRadius: BorderRadius.circular(12.0),
+                return Padding(
+                  padding: EdgeInsets.only(bottom:32.0),
+                  child: Material(
+                    //borderRadius: BorderRadius.circular(12.0),
                     elevation: 14.0,
                     shadowColor: Colors.grey.withOpacity(0.6),
-                    child: Container(
-                      child: Image.network(
-                        posts[index].imageUrl,
-                        fit: BoxFit.cover,
-                        )
+                    child: Stack(
+                      children: <Widget>[
+                      AspectRatio(
+                        aspectRatio: 16/9,
+                        child: Container(
+                            child: Image.network(
+                            posts[index].imageUrl,
+                            fit: BoxFit.cover,
+                           )
+                        ),
+                      ),
+                      Positioned(
+                        left: 12,
+                        top: 20,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              posts[index].title,
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white
+                              ),
+                            ),
+                            Text(
+                              posts[index].author,
+                              style: TextStyle(
+                                fontSize: 13.0,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                      ]
+                      
                     ),
-                  );
+                  ),
+                );
               },
               childCount: posts.length
               ),
